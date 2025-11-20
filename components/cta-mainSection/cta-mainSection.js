00d8f8010230e00ec554855a33a2584f69ac2975
Alone107 <AloneOmen@yandex.ru>
2025-11-20 17:18:05 +0500

    fix_m

--- components/cta-mainSection/cta-mainSection.js
+++ components/cta-mainSection/cta-mainSection.js
@@ -10,3 +10,29 @@ const multiDefault = () => {
 };
 
 multiDefault();
+
+const swiperServicesLeft = document.querySelector(".swiper-services-left");
+
+if (swiperServicesLeft) {
+  const swiperServicesLeft = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "vertical",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+}
