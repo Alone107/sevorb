e2305c90914d7f8474d385818cb488dd253ef665
Alone107 <AloneOmen@yandex.ru>
2025-11-20 18:48:18 +0500

    slide

--- components/services-whiteSection/services-whiteSection.js
+++ components/services-whiteSection/services-whiteSection.js
@@ -6,11 +6,54 @@ if (swiperServicesLeft) {
     loop: true,
     slidesPerView: 1.4,
     spaceBetween: 26,
-
-    speed: 10000,
-
+    speed: 4000,
     autoplay: {
       delay: 0,
+      disableOnInteraction: false,
+    },
+
+    breakpoints: {
+      767: {
+        slidesPerView: 2.4,
+        direction: "vertical",
+        loop: true,
+        spaceBetween: 26,
+
+        speed: 4000,
+        autoplay: {
+          delay: 0,
+          disableOnInteraction: false,
+        },
+      },
+    },
+  });
+}
+
+const swiperServicesRight = document.querySelector(".swiper-services-right");
+if (swiperServicesRight) {
+  const swiperServicesRight = new Swiper(".swiper-services-right", {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1.4,
+    spaceBetween: 26,
+    speed: 4000,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      767: {
+        direction: "vertical",
+        loop: true,
+        slidesPerView: 2.4,
+        spaceBetween: 26,
+        speed: 4000,
+        autoplay: {
+          delay: 0,
+          disableOnInteraction: false,
+          reverseDirection: true,
+        },
+      },
     },
   });
 }
