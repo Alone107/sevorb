b1895861a11bb874b3f79524850b80024fcdc372
Alone107 <AloneOmen@yandex.ru>
2025-11-20 18:05:57 +0500

    fix save

--- components/cta-mainSection/cta-mainSection.js
+++ components/cta-mainSection/cta-mainSection.js
@@ -10,22 +10,3 @@ const multiDefault = () => {
 };
 
 multiDefault();
-
-const swiperServicesLeft = document.querySelector(".swiper-services-left");
-
-if (swiperServicesLeft) {
-  const swiperServicesLeft = new Swiper(".swiper-services-left", {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 1.4,
-    spaceBetween: 26,
-
-    // Настройка скорости и плавности
-    speed: 3000,
-
-    // Автоматическая прокрутка
-    autoplay: {
-      delay: 100,
-    },
-  });
-}
